d0d8b0ef5a6afb31e6cc55784708382609decb9e
Le Quang Trong Nghia <lqtnghia2602@gmail.com>
2024-10-01 14:48:07 +0700

    luyện tập js phần 18.

--- 18. DOM - Gallery ảnh/main.js
+++ 18. DOM - Gallery ảnh/main.js
@@ -22,16 +22,39 @@ let container = document.querySelector("#container");
 
 //Bài toán 3 : thêm 1 thẻ div có 2 phần là img và span chứa số thứ tự
 
+// for (let i = 0; i < 150; i++) {
+//   let div = document.createElement("div");
+//   div.classList.add("info");
+//   container.appendChild(div);
+//   let img = document.createElement("img");
+//   let baseUrl = `https://raw.githubusercontent.com/GaLaiLapTrinh/pokemon/main/img/${
+//     i + 1
+//   }.png`;
+//   img.src = baseUrl;
+//   let span = document.createElement("Span");
+//   span.innerText = `${i + 1}.png`;
+//   div.append(img, span);
+// }
+
 for (let i = 0; i < 150; i++) {
   let div = document.createElement("div");
   div.classList.add("info");
-  container.appendChild(div);
   let img = document.createElement("img");
+  let span = document.createElement("span");
+  span.innerText = `${i + 1}.png`;
   let baseUrl = `https://raw.githubusercontent.com/GaLaiLapTrinh/pokemon/main/img/${
     i + 1
   }.png`;
   img.src = baseUrl;
-  let span = document.createElement("Span");
-  span.innerText = `${i + 1}.png`;
   div.append(img, span);
+  container.append(div);
 }
+
+// bắt sự kiện khi lick vào thì đổi màu
+let listDiv = document.querySelectorAll(".info");
+listDiv.forEach((div) => {
+  div.addEventListener("click", () => {
+    div.classList.toggle("change-bg");
+    div.classList.toggle("pop");
+  });
+});
